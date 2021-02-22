c3cc79a99831818bd14148c9e4b81ebb9ff8f019
Jose Vte. Díaz Mora <74594028+JVDMKKHHPMY@users.noreply.github.com>
2021-02-22 18:07:14 +0100

    Add git ignore

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const db = require('./db');
+const port = 3000;
+const routerFilms = require('./routes/movieRouter')
+
+// Middleware
+app.use(express.json())
+
+app.use(routerFilms);
+
+
+// Start server
+db
+.then(() => {
+    app.listen(port, () => console.log(`Node server running on http://localhost:${port}`));
+})
+.catch((err) => console.log(err.message));
